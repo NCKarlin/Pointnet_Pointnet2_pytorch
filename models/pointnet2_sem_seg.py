14ad0c21b8e03f8297f2463f79cdffde3c3027cf
Niklas Karlin <niklas.karlin@innolidix.com>
2023-08-22 10:20:02 +0200

    SetUp "effortless-haze-219" | 6 SA-\FP-layers

--- models/pointnet2_sem_seg.py
+++ models/pointnet2_sem_seg.py
@@ -44,26 +44,34 @@ class get_model(nn.Module):
                                           sa_mlps[4][-1] + 3, #1024 + 3
                                           sa_mlps[5], 
                                           False)
-        
+        """ self.sa7 = PointNetSetAbstraction(ncentroids[6], 
+                                          radius[6], 
+                                          samples_around_centroid, 
+                                          sa_mlps[5][-1] + 3, #1024 + 3
+                                          sa_mlps[6], #2048
+                                          False)
+         """
         # FEATURE PROPAGATION
-        self.fp6 = PointNetFeaturePropagation(sa_mlps[-1][-1] + sa_mlps[-2][-1], #1024 + 1024
-                                              fp_mlps[0]) 
-        self.fp5 = PointNetFeaturePropagation(sa_mlps[-2][-1] + sa_mlps[-3][-1], #1024 + 512
+        """ self.fp7 = PointNetFeaturePropagation(sa_mlps[-1][-1] + sa_mlps[-2][-1], #2048 + 1024
+                                              fp_mlps[0]) """
+        self.fp6 = PointNetFeaturePropagation(sa_mlps[-2][-1] + sa_mlps[-3][-1], #1024 + 1024
                                               fp_mlps[1]) 
-        self.fp4 = PointNetFeaturePropagation(sa_mlps[-3][-1] + sa_mlps[-4][-1], #512 + 256
+        self.fp5 = PointNetFeaturePropagation(sa_mlps[-3][-1] + sa_mlps[-4][-1], #1024 + 512
                                               fp_mlps[2]) 
-        self.fp3 = PointNetFeaturePropagation(sa_mlps[-4][-1] + sa_mlps[-5][-1], #256 + 128
+        self.fp4 = PointNetFeaturePropagation(sa_mlps[-4][-1] + sa_mlps[-5][-1], #512 + 256
                                               fp_mlps[3]) 
-        self.fp2 = PointNetFeaturePropagation(sa_mlps[-4][-1] + sa_mlps[-6][-1], #256 + 64
-                                              fp_mlps[4])
-        self.fp1 = PointNetFeaturePropagation(sa_mlps[-5][-1], 
+        self.fp3 = PointNetFeaturePropagation(sa_mlps[-5][-1] + sa_mlps[-6][-1], #256 + 128
+                                              fp_mlps[4]) 
+        self.fp2 = PointNetFeaturePropagation(sa_mlps[-5][-1] + sa_mlps[-7][-1], #256 + 64
                                               fp_mlps[5])
+        self.fp1 = PointNetFeaturePropagation(sa_mlps[-6][-1], 
+                                              fp_mlps[6])
         # CLASSIFICATION
-        self.conv1 = nn.Conv1d(sa_mlps[-5][-1], sa_mlps[-5][-1], 1)
-        self.bn1 = nn.BatchNorm1d(sa_mlps[-5][-1])
+        self.conv1 = nn.Conv1d(sa_mlps[-6][-1], sa_mlps[-6][-1], 1)
+        self.bn1 = nn.BatchNorm1d(sa_mlps[-6][-1])
         if dropout:
             self.drop1 = nn.Dropout(dropout_prob)
-        self.conv2 = nn.Conv1d(sa_mlps[-5][-1], num_classes, 1)
+        self.conv2 = nn.Conv1d(sa_mlps[-6][-1], num_classes, 1)
 
 
     def forward(self, xyz, loss_function, dropout):
@@ -75,7 +83,9 @@ class get_model(nn.Module):
         l3_xyz, l3_points = self.sa3(l2_xyz, l2_points)
         l4_xyz, l4_points = self.sa4(l3_xyz, l3_points)
         l5_xyz, l5_points = self.sa5(l4_xyz, l4_points)
+        l6_xyz, l6_points = self.sa6(l5_xyz, l5_points)
         # FEATURE PROPAGATION
+        l5_points = self.fp6(l5_xyz, l6_xyz, l5_points, l6_points)
         l4_points = self.fp5(l4_xyz, l5_xyz, l4_points, l5_points)
         l3_points = self.fp4(l3_xyz, l4_xyz, l3_points, l4_points)
         l2_points = self.fp3(l2_xyz, l3_xyz, l2_points, l3_points)
