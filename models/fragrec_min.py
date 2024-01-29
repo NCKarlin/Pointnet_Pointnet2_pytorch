ab837131b2aed24ee00d4978b0e517a28bf5ccb3
Niklas Karlin <niklas.karlin@innolidix.com>
2024-01-29 16:16:01 +0100

    adjustment of import location for layer classes

--- models/fragrec_min.py
+++ models/fragrec_min.py
@@ -1,6 +1,6 @@
 import torch.nn as nn
 import torch.nn.functional as F
-from models.pointnet2_utils import PointNetSetAbstractionMsg,PointNetFeaturePropagation
+from models.fragrec_utils import PointNetSetAbstractionMsg,PointNetFeaturePropagation
 
 class get_model(nn.Module):
     def __init__(self, 
