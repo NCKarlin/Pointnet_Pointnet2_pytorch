1802b9e6eab877033221b2826f7be26323bdb591
Niklas Karlin <niklas.karlin@innolidix.com>
2024-02-14 11:32:34 +0100

    Adjusting commenting to google style

--- data_preparation/data_prep_utils.py
+++ data_preparation/data_prep_utils.py
@@ -5,17 +5,18 @@ import open3d as o3d
 
 
 def create_rgb_list(input_pc):
-    '''Function to create RGB-list for point cloud creation and display
-    
+    """Function to create RGB-list for point cloud creation and display
+
     The RGB-list has to be handed over individually from the point coordinates for the 
     visualization of a point cloud, hence this function for its individual creation. 
     N: number of points | F: number of features per point
-    
-    :param input_pc: input point cloud data with RGB-info in columns 3, 4, 5
-    :type input_pc: numpy.ndarray [N, F]
-    :return rgb_list: list of normalized colors corresponding to input pc points
-    :rtype: numpy.ndarray [N, 3]
-    '''
+
+    Args:
+        input_pc (np.ndarray): input point coud with RGB values in column 3-5 [N, F]
+
+    Returns:
+        np.ndarray: array of normalized RGB values per point [N, 3]
+    """
     # Assigning color channels
     red_c = input_pc[:, 3]
     green_c = input_pc[:, 4]
@@ -30,15 +31,15 @@ def create_rgb_list(input_pc):
 
 
 def makePC(point_data, color_data=np.array([])):
-    '''Function to create an open3d point cloud object from point cloud data
-    
-    N: number of points | F: number of features per point
-    
-    :param point_data: array containing coordinates and RGB data of all points of pc 
-    :type point_data: numpy.ndarray [N, F]
-    :return pcd: open3d point cloud data object from point_data
-    :rtype: open3d.geometry.PointCloud
-    '''
+    """Function to create an open3d point cloud object from point cloud data
+
+    Args:
+        point_data (np.ndarray): array containing coordinates and RGB data of all points of pc [N, F]
+        color_data (np.ndarray): array containing per point RGB data [N, 3]
+
+    Returns:
+        open3d.geometry.PointCloud: open3d point cloud data object from input data
+    """
     pcd = o3d.geometry.PointCloud() # Create PC object
     pcd.points = o3d.utility.Vector3dVector(point_data) # Give coordinates
     # Coloring the PC
@@ -50,15 +51,15 @@ def makePC(point_data, color_data=np.array([])):
 
 
 def get_lowest_corner_coords(cuboid_coords, axis):
-    ''' Retrieving coordinates of lowest cuboid corner in respective direction
-    
-    :param cuboid_coords: array of coordinates of cuboid corners
-    :type cuboid_coords: numpy.ndarray [8, 3]
-    :param axis: string of specified axis/ direction for minimum point value
-    :type axis: string
-    :return lowest_corner_coords: xyz-coordinates of lowest corner in specified direction
-    :rtype: numpy.ndarray [1, 3]
-    '''
+    """Retrieving coordinates of lowest cuboid corner in respective direction
+
+    Args:
+        cuboid_coords (np.ndarray): array of coordinates of cuboid corners [8, 3]
+        axis (str): string specifying axis/ direction for minimum point value
+
+    Returns:
+        np.ndarray: lowest_corner_coords - xyz-coordinates of lowest corner in specified axis/ direction
+    """
     if axis == 'z':
         z_val_lowest_corner = np.min(cuboid_coords[:,2])
         # Correction in the case there are several corners with the same value
@@ -87,16 +88,16 @@ def get_lowest_corner_coords(cuboid_coords, axis):
 
 
 def lowest_corner_to_all_other_corners_vecs(bbox_points, lowest_corner_coords):
-    '''Function to ceate all 7 vectors from the lowest corner to all other cuboid-corners
-    
-    :param bbox_points: array of coords of all 8 cuboid corner points [8, 3]
-    :type bbox_points: numpy.ndarray [8, 3]
-    :param lowest_corner_coords: coords of chosen lowest corner of cuboid coords [1, 3]
-    :type lowest_corner_coords: numpy.ndarray [1, 3]
-    :return lowest_corner_to_corner_vectors_list: array of all vectors from lowest corner
-    to all others
-    :rtype: numpy.ndarray [7, 3]
-    '''
+    """Function to ceate all 7 vectors from the lowest corner to all other cuboid-corners
+
+    Args:
+        bbox_points (np.ndarray): array of coordinates of all 8 cuboid corner points [8, 3]
+        lowest_corner_coords (np.ndarray): coords of chosen lowest corner of cuboid coords
+
+    Returns:
+        np.ndarray: lowest_corner_to_corner_vectors_list - array of all vectors from lowest
+                    to all others
+    """
     lowest_corner_to_corner_vectors_list = []
     for i in range(len(bbox_points)):
         vec_i = bbox_points[i] - lowest_corner_coords
@@ -109,19 +110,19 @@ def lowest_corner_to_all_other_corners_vecs(bbox_points, lowest_corner_coords):
 
 def create_orthogonal_vector_list(shortest_lowest_corner_to_corner_vector_coords, 
                                   lowest_corner_to_corner_vecs):
-    ''' Function to determine orthogonal corner vectors
-    
+    """Function to determine orthogonal corner vectors
+
     Given the vectors from the lowest corner to all other corners, as well as the shortest
     lowest corner to corner vectors, this function determines the other two orthogonal 
-    vectors, which meet at the lowest corner of the cuboid. 
-    
-    :param shortest_lowest_corner_to_corner_vector_coords: see name
-    :type shortest_lowest_corner_to_corner_vector_coords: numpy.ndarray [1, 3]
-    :param lowest_corner_to_corner_vecs: see name 
-    :type lowest_corner_to_corner_vecs: numpy.ndarray [7, 3]
-    :return orthogonal_vec_coords_list: list of 3 orthogonal vectors at lowest corner
-    :rtype: class 'list'
-    '''
+    vectors, which meet at the lowest corner of the cuboid.
+
+    Args:
+        shortest_lowest_corner_to_corner_vector_coords (np.ndarray): [1, 3]
+        lowest_corner_to_corner_vecs (np.ndarray): [7, 3] 
+
+    Returns:
+        list: orthogonal_vec_coords_list - list of 3 orthogonal vectors at lowest corner
+    """
     orthogonal_vec_coords_list = []
     for i in range(len(lowest_corner_to_corner_vecs)):
         # Orthogonality check through dot product of unit vectors 
@@ -135,35 +136,38 @@ def create_orthogonal_vector_list(shortest_lowest_corner_to_corner_vector_coords
 
 
 def norm_vec(vector):
-    ''' Normalize given vector, this function was mainly crated for readability
-    
-    :param vector: any 3D-vector/ coords combination
-    :type vector: numpy.ndarray [1, 3]
-    :return: vector norm
-    :rtype: float
-    '''
+    """Normalize given vector, this function was mainly crated for readability
+
+    Args:
+        vector (np.ndarray): any 3D-vector/ coordinate combination
+
+    Returns:
+        float: vector norm
+    """
     return np.linalg.norm(vector)
 
 
 def unit_vector(vector):
-    ''' Create unit-vector for given vector, was mainly created for readability
-    
-    :param vector: 3D-vector / coords combination
-    :type vector: numpy.ndarray [1, 3]
-    :return: unit vector of given vector
-    :rtype: numpy.ndarray [1, 3]
-    '''
+    """Create unit-vector for given vector, was mainly created for readability
+
+    Args:
+        vector (np.ndarray): 3D-vector coordinates
+
+    Returns:
+        np.ndarray: unit vector of given input vector 
+    """
     return vector / np.linalg.norm(vector)
 
 
 def create_vectors(start_end_point_array):
-    ''' Create vector array from start and end point array
-    
-    :param start_end_point_array: array of start and end point for vectors 
-    :type start_end_point_array: numpy.ndarray [Num_vectors, 2, 3]
-    :return vec_return: array of vectors from given start to end point
-    :rtype: numpy.ndarray [Num_vectors, 3]
-    '''
+    """Create vector array from start and end point array
+
+    Args:
+        start_end_point_array (np.ndarray): array of start and end points for vectors [N, 2, 3]
+
+    Returns:
+        np.ndarrray: array of vectors from given start to end points [N, 3]
+    """
     vec_return = np.zeros((len(start_end_point_array), 3))
     for i in range(len(start_end_point_array)):
         vec_i = start_end_point_array[i][1] - start_end_point_array[i][0]
@@ -172,13 +176,14 @@ def create_vectors(start_end_point_array):
 
 
 def create_vec_distances(vectors):
-    ''' Compute vector distances for given array of vectors
-    
-    :param vectors: array of 3D vectors
-    :type vectors: numpy.ndarray [N, 3]
-    :return vector_dists: array of vector distances
-    :rtype: numpy.ndarray [N, 1]
-    '''
+    """Compute vector distances for given array of vectors
+
+    Args:
+        vectors (np.ndarray): array of 3D-vectors [N, 3]
+
+    Returns:
+        np.ndarray: array of vector distances [N, 1]
+    """
     vector_dists_list = []
     for i in range(len(vectors)):
         vector_i_dist = norm_vec(vectors[i])
@@ -187,15 +192,16 @@ def create_vec_distances(vectors):
     return vector_dists
 
 def get_orthonormal_vec_coords(orthonnormal_vec_coords, which):
-    ''' Select specified vector coordinates from orthogonal vector coordinate array
-    
-    :param orthonormal_vec_coords: list/ array of 3D orthogonal vectors
-    :type orthonormal_vec_coords: numpy.ndarray [3, 3]
-    :param which: string decoding whether to get 'longest', 'middle' or 'shortest' orthogonal vec
-    :type which: string
-    :return: 3D vector coordinates of specified orthogonal vector
-    :rtype: numpy.ndarray [1, 3]
-    '''
+    """Select specified vector coordinates from orthogonal vector coordinate array
+
+    Args:
+        orthonnormal_vec_coords (np.ndarray): array of 3D orthogonal vectors [3, 3]
+        which (str): string decoding whether to get 'shortest', 'middle' or 'longest'
+                     orthogonal vector
+
+    Returns:
+        np.ndarray: 3D vector coordinates of specified orthogonal vector
+    """
     orthogonal_vec_dists = create_vec_distances(orthonnormal_vec_coords)
     # Sort coordinate list as the distance list 
     dist_sorted_indices = orthogonal_vec_dists.argsort()
@@ -212,15 +218,15 @@ def get_orthonormal_vec_coords(orthonnormal_vec_coords, which):
 
 
 def rot_mat_from_vecs(vec1, vec2):
-    ''' Find rot-mat for aligning vec1 with vec 2 (based on Rodrigues' rotation formula)
-    
-    :param vec1: 3D vector coordinates of vector to be rotated
-    :type vec1: numpy.ndarray [1, 3]
-    :param vec2: 3D vector coordinates of vector to be rotated onto
-    :type vec2: numpy.ndarray [1, 3] 
-    :return rot_mat: Rotation matrix for rotating vec1 onto vec2
-    :rtype: numpy.ndarray [3, 3]
-    '''
+    """Find rot-mat for aligning vec1 with vec 2 (based on Rodrigues' rotation formula)
+
+    Args:
+        vec1 (np.ndarray): 3D vectors coordinates of vector to be rotated
+        vec2 (np.ndarray): 3D vector coordinates of vector to be rotated onto
+
+    Returns:
+        np.ndarray: Rotation matrix for rotating vec1 onto vec2
+    """
     vec1_uv, vec2_uv = unit_vector(vec1).reshape(3), unit_vector(vec2).reshape(3)
     v = np.cross(vec1_uv, vec2_uv)
     c = np.dot(vec1_uv, vec2_uv)
@@ -233,15 +239,15 @@ def rot_mat_from_vecs(vec1, vec2):
 
 
 def rotate_coords_onto_axis(coords, R):
-    ''' Function that rotates coords according to given rotation-matrix R
-    
-    :param coords: coordinates of N points to be rotated
-    :type coords: numpy.ndarray [N, 3]
-    :param R: rotation-matrix to be applied to coordinates
-    :type R: numpy.ndarray [3, 3]
-    :return coords_rot: new rotated coordinates of input coordinates
-    :rtype: numpy.ndarray [N, 3]
-    '''
+    """Function that rotates coords according to given rotation-matrix R
+
+    Args:
+        coords (np.ndarray): 3D coordinates of N points to be rotated [N, 3]
+        R (np.ndarray): rotation-matrix ot be applied to coordinates [3, 3]
+
+    Returns:
+        np.ndarray: new rotated coordinates of input coordinates [N, 3]
+    """
     # Depending on shape of coordinates passed rotation multiplication adjustment
     if coords.shape == (3,):
         coords_rot = R.dot(coords)
